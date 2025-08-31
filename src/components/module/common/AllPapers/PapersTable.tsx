8df1765de2acfe2dca3fb36034dbaadf196589c6
jhimi01 <talukderjhimi@gmail.com>
2025-08-31 16:59:50 +0600

    handle empty data page

--- src/components/module/common/AllPapers/PapersTable.tsx
+++ src/components/module/common/AllPapers/PapersTable.tsx
@@ -55,10 +55,7 @@ const PapersTable = ({ papers }: PapersTableProps) => {
             <h3 className="text-lg font-medium text-gray-900 mb-2">
               No papers found
             </h3>
-            <p className="text-gray-600">
-              Try adjusting your search or filters to find what you're looking
-              for.
-            </p>
+           
           </div>
         </CardContent>
       </Card>
