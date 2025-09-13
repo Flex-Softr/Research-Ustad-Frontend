edf16006160a36ce1c12aafa10a1f07db1c219f8
jhimi01 <talukderjhimi@gmail.com>
2025-09-13 23:40:43 +0600

    added location map

--- src/app/(WithComonLayout)/contact/page.tsx
+++ src/app/(WithComonLayout)/contact/page.tsx
@@ -11,7 +11,7 @@ const page = () => {
         </h3>
         <div className="h-96 bg-muted rounded-xl overflow-hidden">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d233667.8223908687!2d90.25446891259788!3d23.780863188152252!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8b087026b81%3A0x8fa563bbdd5904c2!2sDhaka!5e0!3m2!1sen!2sbd!4v1620734414401!5m2!1sen!2sbd"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3368.129051983705!2d-81.77615672435219!3d32.415701073816706!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88fa473206d2e165%3A0xad1ab923707937b6!2s100%20Bermuda%20Run%20Rd%2C%20Statesboro%2C%20GA%2030458%2C%20USA!5e0!3m2!1sen!2sbd!4v1757784995460!5m2!1sen!2sbd"
             width="100%"
             height="100%"
             style={{ border: 0 }}
@@ -21,7 +21,7 @@ const page = () => {
           ></iframe>
         </div>
         <p className="text-center text-gray-600 mt-4 text-xl">
-          Based in Dhaka, Bangladesh. Available for remote work worldwide.
+          Available for remote work worldwide.
         </p>
       </section>
     </div>
